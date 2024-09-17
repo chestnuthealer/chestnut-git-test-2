1b6c2a2f10477712cbfb308a90596e5faa34cf8b
ChestnutHealer <chestnuthealer@gmail.com>
2024-09-17 17:07:23 +0700

    Add list in file index

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis quia repellendus dolorem impedit doloremque eius, molestiae in voluptas autem dolorum mollitia laudantium facere voluptates! Enim, nostrum! Aut velit ut inventore.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
